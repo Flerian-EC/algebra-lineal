9cecf83511f2ca864b6d649fafc0a52b853ccb5d
Fabian Juel Escalera Caballero <fabianjuelescalera@gmail.com>
2024-01-26 13:40:58 -0400

    2 version

--- script.js
+++ script.js
@@ -4,11 +4,35 @@ const $botonCalc = document.getElementById("calc-button");
 const $numeroHexa = document.getElementById("hexa-number");
 const $bloqueColor = document.getElementById("principal-color");
 
+const $matriz = document.querySelector("#container-matriz");
+
+
 const obtenerNumero = () => {
   let value = $numeroHexa.value;
   return value
 }
-let x = "1"
+
+const validarNum = (val) => {
+  let confirm = isNaN(val)
+  return !confirm
+}
+
+const renderMatriz = ({validacion, dimension}) => {
+  $matriz.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`
+  $matriz.style.gridTemplateRows = `repeat(${dimension}, 1fr)`
+  let dim = dimension ** 2 
+
+  const inputMat = (iden) => {
+    return `<input matrizId=${iden.toString()} class="entrada-de-matriz" type="number">`;
+  } 
+  const list = [];
+  for(let i = 0; i < dim; i++) {
+    list.push(inputMat(i+1));
+  }
+
+  $matriz.innerHTML = `${list.join(" ")}`
+}
+
 const animar = (validacion) => {
   if(validacion === true) {
     $bloqueColor.classList.add("animar-color");
@@ -18,7 +42,7 @@ const animar = (validacion) => {
 //esta es la ejecucion que se debe modificar: 
 $botonCalc.addEventListener("mouseup", (event) => {
   let value = obtenerNumero()
-  let validar = validarHexa(value);
-  cambio({validacion: validar, valor: value});
+  let validar = validarNum(value);
+  renderMatriz({validar: validar, dimension: value})
   animar(validar);
 })
